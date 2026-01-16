c6781f2c58bc150c0db8c022e7efb65d396ac1b9
Nadimo <42928954+nstaszak@users.noreply.github.com>
2026-01-16 16:06:20 +0100

    Remove vendor directory from .gitignore and add Composer autoload files
    The vendor directory was removed from .gitignore, and Composer's autoload and related files were added to version control. This change ensures that Composer's autoloader and metadata are included in the repository directly.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'uzk/xial',
+        'pretty_version' => 'dev-trunk',
+        'version' => 'dev-trunk',
+        'reference' => '18eb93b4425b9e46bbe2253da984af73e040c8a5',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'uzk/xial' => array(
+            'pretty_version' => 'dev-trunk',
+            'version' => 'dev-trunk',
+            'reference' => '18eb93b4425b9e46bbe2253da984af73e040c8a5',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
